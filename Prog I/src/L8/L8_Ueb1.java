65ffd0651559307fb272f1f074548f7ade98f587
Mahmutekremacar <mahmutekremacar@gmail.com>
2023-12-05 17:41:00 +0100

    .
    nicht erl versuch von mir fuer l8ueb1

--- Prog I/src/L8/L8_Ueb1.java
+++ Prog I/src/L8/L8_Ueb1.java
@@ -1,36 +1,58 @@
 package L8;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+
 public class L8_Ueb1 {
-/*
-What we need
-lives var
-array of sentence
-continuous input
-* continuous loop that checks if the given char is in the array or not (if in array reloads the output accordingly.
-* If not decreases lives one from total of 20. )
-* */
-public static char[] Space(char[] input){
-    char[] total;
+    /*
+    What we need
+    lives var
+    array of sentence
+    continuous input
+    * continuous loop that checks if the given char is in the array or not (if in array reloads the output accordingly.
+    * If not decreases lives one from total of 20. )
+    * */
 
-    for(char ch: input){
-        if(ch==' '){
-            ch = 'h'+'i';
-            total += ch;
+    public static void transposerToGame(char[] arr) {
+        CharArrayWriter caw = new CharArrayWriter();
+        for (int i = 0; i < arr.length; i++) {
+            caw.append('_');
+
         }
-        else{
-            total += ch;
-        }}
-
-        return total;
-    public static void main(String[] args)
-    {
-        String saetze = "PROGRAMMIERSPRACHE";
-        char[] arr = saetze.toCharArray();
-        //System.out.println(arr[0]);
-
-        char[] total;
-
-
+        System.out.println(caw);
 
     }
+
+    static void dieSpiel(char[] arrWord, char[] wordPlace, int lives) {
+        Scanner sc = new Scanner(System.in);
+
+
+        Boolean finished = false;
+        while (lives > 0 && finished == false) {
+            System.out.println("Errate einen buchstaben: ");
+            char erratung = sc.next().charAt(0);
+            int contains = 0;
+            for (char c : arrWord) {
+
+                if (c == erratung) {
+                    wordPlace.replace(wordPlace[erratung],erratung);
+
+                }
+                contains++;
+
+            }
+        }
+    }
+
+        public static void main (String[]args)
+        {
+            String saetze = "PROGRAMMIERSPRACHE";
+            char[] arr = saetze.toCharArray();
+            System.out.println(arr);
+            transposerToGame(arr);
+
+
+        }
+
 }
+
