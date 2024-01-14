815571210303cf1d3fcc304787538c9458abe8d4
Mahmutekremacar <mahmutekremacar@gmail.com>
2024-01-14 23:52:36 +0100

    L1 Ueb1 erl

--- Prog_restudy/src/L1/Ueb1.java
+++ Prog_restudy/src/L1/Ueb1.java
@@ -1,6 +1,12 @@
 package L1;
 
 public class Ueb1 {
-
-
+    public static void main(String[] args) throws InterruptedException {
+        int num = 0;
+        for (int i = 15; i >= num; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
 }
+//erl
