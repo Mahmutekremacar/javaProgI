867ef133a5345112afaa8c2f766c3f756f3dd2c0
Mahmutekremacar <mahmutekremacar@gmail.com>
2024-02-25 22:10:34 +0100

    ~

--- Prog_restudy/src/Algorithmen/selectionSort.java
+++ Prog_restudy/src/Algorithmen/selectionSort.java
@@ -1,8 +1,26 @@
 package Algorithmen;
-
+import java.util.*;
 public class selectionSort {
+    private static int[] selectSort(int[] numbers)
+    {
+
+    }
     public static void main(String[] main)
     {
-        
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0 ; i < numbers.length; i++)
+        {
+            numbers[i] = rand.nextInt(100000);
+        }
+        System.out.println("\n list Nums:");
+        System.out.println(Arrays.toString(numbers));
+
+        //selectionSort(numbers);
+        System.out.println("\n (Selection Sorted) new list Nums:");
+        System.out.println( Arrays.toString(selectSort(numbers)));
+
+
     }
 }
